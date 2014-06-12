62888906d6750328975d74c4dc161d8b2a138a58
arvimurthy <arvimurthy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-12 08:32:01 +0000

    Chatimity Chat Rooms plugin first version
    git-svn-id: https://plugins.svn.wordpress.org/chatimity-chat-rooms/trunk@930932 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ chatimity-widget.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Plugin Name: Chatimity Chat Rooms
+ * Plugin URI: http://chatimity.com/wp/chatimity-widget.zip
+ * Description: Widget for visitors to chat using chatimity on a particular topic.
+ * Version: 1.0
+ * Author: Chatimity Software Pvt Ltd
+ * Author URI: http://chatimity.com
+ *
+ */
+
+add_action( 'widgets_init', 'chatimity_load_widgets' );
+
+/**
+ * Register widget.
+ */
+function chatimity_load_widgets() {
+    register_widget( 'Chatimity_Widget' );
+}
+
+/**
+ * Chatimity Widget class.
+ * Adds the html to load chatimity widget.
+ *
+ * @since 0.1
+ */
+class Chatimity_Widget extends WP_Widget {
+
+    function Chatimity_Widget() {
+        $widget_ops = array( 'classname' => 'chatimity', 'description' => __('Widget for visitors to chat using chatimity on a particular topic', 'chatimity') );
+
+        $control_ops = array( 'width' => 250, 'height' => 400, 'id_base' => 'chatimity-widget' );
+
+        $this->WP_Widget( 'chatimity-widget', __('Chatimity Chat Rooms', 'chatimity'), $widget_ops, $control_ops );
+    }
+
+    function widget( $args, $instance ) {
+        extract( $args );
+
+        $topics = $instance['topics'];
+        $title = $instance['title'];
+
+        echo $before_widget;
+
+        printf( '<script type="text/javascript">' );
+        printf( '    (function() {' );
+        printf( '        var e = document.createElement("script"); e.type = "text/javascript"; e.async = true;' );
+        printf( '        e.src = "https://secure.chatimity.com/chat.js";' );
+        printf( '        var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(e, s);' );
+        printf( '    }());' );
+        printf( '</script>' );
+        printf( '<div id="chatimity_widget" style="position:fixed;z-index:99;bottom:0;right:20px;">' );
+        printf( '    <iframe id="chatimity-ifr" src="https://secure.chatimity.com/widget?ct=6&cv=2&title=%s&topics=%s" width="250px" height="400px" scrolling="no" frameborder="0"></iframe>', urlencode($title), $topics );
+        printf( '</div>' );
+        printf( '<div id="chatimity_widget" style="position:fixed;bottom:0;right:20;">' );
+
+        echo $after_widget;
+
+    }
+
+	/**
+	 * Update the widget settings.
+	 */
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['topics'] = strip_tags( $new_instance['topics'] );
+        return $instance;
+    }
+
+    function form( $instance ) {
+
+        /* Set up some default widget settings. */
+        $defaults = array( 'title' => __('Fifa Chat', 'chatimity'), 'topics' => __('FIFA', 'chatimity') );
+        $instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+        <!-- Widget Title -->
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'chatimity'); ?></label>
+            <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+        </p>
+
+        <!-- Widget Topic -->
+        <p>
+            <label for="<?php echo $this->get_field_id( 'topics' ); ?>"><?php _e('Topics:', 'chatimity'); ?></label>
+            <input id="<?php echo $this->get_field_id( 'topics' ); ?>" name="<?php echo $this->get_field_name( 'topics' ); ?>" value="<?php echo $instance['topics']; ?>" style="width:100%;" />
+        </p>
+
+    <?php
+    }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Chatimity Chat Rooms ===
+Contributors: aravind
+Tags: chat, chat online, chat rooms, free chat, chat software, user engagement, IM chat,
+live chat, live chat software, live chatting, time on site, live web chat, online chat,
+website chat, wordpress chat, wordpress live chat, wordpress live chat plugin,
+Chatimity
+Requires at least: 3.1
+Tested up to: 3.9.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Chatimity Chat Rooms helps website owners to keep their users engaged and active on site! And it's Free!
+
+== Description ==
+
+Increasing the user engagement and time spent on your website is now just a click away by using <a href="http://www.chatimity.com/">Chatimity</a> plugin.
+
+Chatimity Chat Rooms appear as a chatbar docked at the bottom of your website. Visitors can instantaneously see activity in the chat room corresponding to a topic of your choice. They can also chat with other users after a simple email signup.
+Adding this widget to your website would immediately make it look more dynamic and lively. Visitors who signup with the widget can engage with the already million plus thriving community in Chatimity and increase their time spent on your site.
+
+We use state of the art technology and advanced algorithms to keep the widget very fast and free of spam. And the best part is it is completely free of cost irrespective of the number of users who use the widget.
+
+Love to know more about the product? Please visit our <a href="http://www.chatimity.com" target="_blank">website</a> or mail us at contact@chatimity.com.
+
+
+== Changelog ==
+= 1.0 =
+* Launching Chatimity Chat Rooms widget.
+
+== Screenshots ==
+
+1. Chat window on your website - active chat
+2. Account Configuration
+3. User signup
+
+== Frequently Asked Questions ==
+
+= Do I have to install any software on my server to get this working? =
+
+None required! Simply configure the plugin and you're done!
+
+= How much do you charge? =
+
+Chatimity is completely free of cost irrespective of number of users using the widget!
+
+= Its not working =
+
+Please mail us at contact@chatimity.com and we will help you with installation.
+
+== Installation ==
+
+*Server Requirements:* PHP4 or PHP5.
+
+*Wordpress versions:* Wordpress 2.7 and up.
+
+Step-by-step Guide:
+
+* Install plugin from WordPress directory.
+* Activate the plugin through the 'Plugins' menu in WordPress.
+* Visit the Appearence->Widgets menu in Wordpress and choose the title and topic for the chat room shown.
